cf3986a28dcf290d585e90cfab13ca0ade5959be
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-09-09 18:20:16 +0530

    Init

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc)
+{
+    int a, b, c;
+
+    scanf("%d%d", &a, &b);
+
+    c = a + b;
+
+    printf(" the value of a in %d, value of b is %d, and value of c is %d", a, b, c);
+
+    return 0;
+}
